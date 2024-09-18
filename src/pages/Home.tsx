2fc190edecd7c2588671b1777031f7ef68e95e2f
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2024-09-18 19:47:31 -0300

    Upload dos arquivos referente a Landing Page
    Upload dos arquivos que até agora foram criados com apoio e orientação do professor Sandro.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import Close from "../assets/close.svg";
+import Menu from "../assets/menu.svg";
+import Logo from "../assets/logo.svg";
+import "../styles/header.css";
+import "../styles/utility.css";
+import Button from "../components/buttons.tsx";
+import Quadradao from "../assets/images/quadradao.png";
+import Quadradin from "../assets/images/quadradin.png";
+import "../styles/hero.css";
+
+export default function Home() {
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+    return (
+        <>
+            <header className="container py-sm">
+                <nav className="flex items-center justify-between">
+                    <img src={Logo} alt="Logo HowToLive" width={220} height={80} />
+                    <div className="desktop-only">
+                        <ul className="flex gap-1">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#solution">Soluções</a>
+                            </li>
+                            <li>
+                                <a href="#testimonials">Depoimentos</a>
+                            </li>
+                            <li>
+                                <a href="#pricing">Preços</a>
+                            </li>
+                            <li>
+                                <a href="#contact">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="desktop-only">
+                    <div className="flex items-center">
+                            <a className="reverse-color ml-lg" href="">Login</a>
+                            <Button text="Cadastre-se" />
+                        </div>
+                    </div>
+                    <div className="mobile-menu">
+                        {showMobileMenu ?
+                            <div className="mobile-menu-content">
+                                <div className="container flex">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Home</a>
+                                        </li>
+                                        <li>
+                                            <a href="#solution">Soluções</a>
+                                        </li>
+                                        <li>
+                                            <a href="#testimonials">Depoimentos</a>
+                                        </li>
+                                        <li>
+                                            <a href="#pricing">Preços</a>
+                                        </li>
+                                        <li>
+                                            <a href="#contact">Contato</a>
+                                        </li>
+                                        <li>
+                                            <a className="reverse-color" href="#">Login</a>
+                                        </li>
+                                    </ul>
+                                    <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
+                                        <img src={Close} alt="ícone fechar menu" width={24} height={24} />
+                                    </span>
+                                </div>
+                            </div>
+                            :
+                            <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper" >
+                                <img src={Menu} alt="ícone menu" width={24} height={24} />
+                            </span>
+                        }
+                    </div>  
+
+
+                </nav>
+            </header>
+
+            <section id="hero">
+                <span className="desktop-only">
+                    <img src={Quadradin} alt="Retangulozinho" />
+                </span>
+                <img src={Quadradao} alt="Retangulo" />
+                <div className="container content">
+                    <p className="desktop-only">Olá</p>
+                    <h1>
+                        Uma Solução que irá te entregar X
+                    </h1>
+                    <p>
+                        Você sabe que, para alcançar o sucesso, é fundamental ter parceiros que te impulsionem a ir mais longe
+                    </p>
+                    <div>
+                        <span><Button text="Cadastre-se" /></span>
+                        <span className="desktop-only">
+                            <Button text="Veja mais" secondary />
+                        </span>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+    
+}
