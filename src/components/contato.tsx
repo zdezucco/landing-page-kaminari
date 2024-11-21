da05cb4e18ad65da8baec6d6c57db11bc9747ed9
Gabriel Barbosa Zucco <gabrielbzucco@gmail.com>
2024-11-21 19:36:28 -0300

    Atualização do Token

--- src/components/contato.tsx
+++ src/components/contato.tsx
@@ -31,7 +31,7 @@ export default function Contato() {
         headers: {
           'Access-Control-Allow-Origin': '*',
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ5NzQwYTcwYjA5NzJkY2NmNzVmYTg4YmM1MjliZDE2YTMwNTczYmQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA2MTQxMDQ4NDAyODUyMjY4OTc3IiwiZW1haWwiOiJnYWJyaWVsYnp1Y2NvQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoibWZLdGNTR2MxZHRFM0ZHSW5reV9vQSIsIm5iZiI6MTczMjE3MTUzOSwiaWF0IjoxNzMyMTcxODM5LCJleHAiOjE3MzIxNzU0MzksImp0aSI6ImFlMTg5YjllODYyOTEyOGExOTc2NTBiMTUyYTY0ODBmZGY0N2ExZDUifQ.TEB5iFyfczwu_Fgs0XaNch4Dv444bdGt0AF7D1IWcErTtrrcpyBUAzrZVUpzSqVcCzQCJVEr5EuVPqdx1rSYwUsEh_429HVICnrcBsnH1mYy9kNGEfaOk_V0LUC44VZEc37O6GgqnlzGeLcvDgtC10ZJcAYVR0Z7cwA84xmPESOzB3om98Pmktyxmf9fqNkIZEcxsX1HO8KOGNMZe14DWtBYsRMNBauei7U30S4NpwCa2tfD4AAIwbw5ZqXgKE_RJi4ZjD_ObcBejkB48KKP005xw8JlJZCjjtH_NYGxWr9MM5I_BttUZ1ZlVhNXZU3MfqobLqG4DC5rtI2vq12kCA',
+          'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ5NzQwYTcwYjA5NzJkY2NmNzVmYTg4YmM1MjliZDE2YTMwNTczYmQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiYXVkIjoiNjE4MTA0NzA4MDU0LTlyOXMxYzRhbGczNmVybGl1Y2hvOXQ1Mm4zMm42ZGdxLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTA2MTQxMDQ4NDAyODUyMjY4OTc3IiwiZW1haWwiOiJnYWJyaWVsYnp1Y2NvQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiV1ZVdUNtSDBUd0RFaUhJSzBSWTQ4ZyIsIm5iZiI6MTczMjIyODEyOCwiaWF0IjoxNzMyMjI4NDI4LCJleHAiOjE3MzIyMzIwMjgsImp0aSI6Ijk5ODA3ZDUwYTJiM2UwZGNmYTM3OGYyOGY1MjIyOTRkM2JiNWQ0MGMifQ.dH9WlaVsdqJ3OgAEkYPYTtYbSznFVUvzRo3-d2rmA55suCc-f8TFS02zZVokZBgRTjNV2_ea0uRc6iDduyDhvH7rUpCc3uEFwEVPk02US1RqTZhJcvn2ALMvFJwMxRe8P3pE5hpIfxEyLdP2qTJHBy0f-iqH44I_wpEpwNH_8NcT7QfAdvKle95qJbPeD2gaJz1V2rERa1_VfYGCJX34ypj4XN9zPH_xUC-f7ww_MmgwzzrEpKpARvxszhvNWJqeCbvKpmQWjM4IwREj3UKpaZuI3m_xN3S8vr9qjZZR5ZPy_lxSt3PCdQmyDvDqJLwjVl6bMj8hZnw9pyc-mtbHbw',
         },
         body: JSON.stringify({ toMail: email, name }),
       });
@@ -73,7 +73,7 @@ export default function Contato() {
         </span>
           <span id="campos" className='container'> 
             <input type="email" className='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Digite seu e-mail" required />
-            <textarea value={name} className='nome' onChange={(e) => setName(e.target.value)} placeholder="Digite seu nome" required />
+            <input value={name} className='nome' onChange={(e) => setName(e.target.value)} placeholder="Digite seu nome" required />
 
             <button type="submit" className='botaoEnviar' disabled={loading}>{loading ? 'Enviando...' : 'Gostei do Projeto!'}</button>
             {error && <p className="error">{error}</p>}
